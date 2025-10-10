a33e3b0a91db05cec2ff33f2346d2f7d95945d1d
Samuel Boistel <boistelsam@gmail.com>
2025-10-10 08:47:19 +0200

    feat: master script 2 sync

---
+++ updt_docs.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+#############################################################
+# 
+# Author:       Samuel BOISTEL
+# Topic:        Topic
+# Date:         DATE
+# 
+#############################################################
+
+# Build the site
+hugo build
+
+if [ $? -eq 1 ]; then
+    echo "Hugo build failed"
+    exit 1
+fi
+
+# Sync the site to the server
+rsync -rvz public/* draxpi:/opt/docker/land/data/html
+
+[[ $? -eq 1 ]] && echo "Rsync failed" && exit 1 || rm -rf public
+
+echo "Site updated successfully"
+
+# EOF
